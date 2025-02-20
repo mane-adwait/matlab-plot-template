d253fbff8f821c269e35b543a1c83355b2da3ff6
Adwait Mane <mane.learn.2023.04@gmail.com>
2025-02-20 16:16:24 -0500

    New code.

--- plot_template.m
+++ plot_template.m
@@ -3,3 +3,18 @@
 % Dependencies: linspecer.m.
 % Author: Adwait Mane. 2022 January.
 
+%% Generate plots.
+
+% Create the figure and set the properties:
+fig5 = figure(5); % For convenience, use figN = figure(N)
+cFig = gcf; % This makes the following commands modular i.e. the figure 
+% handle does not need to be updated.
+% cFig.Name = [fileName, fileVersion]; 
+
+% Set the figure position using the syntax [left bottom width height].
+cFig.Units = 'normalized'; cFig.OuterPosition = [0.5 0.5 0.5 0.5];
+% movegui(cFig,'northeast'); % Another way to specify figure location.
+
+cFig.Color = 'white'; % I think the 'no background' option appears black in
+% the Matlab environment, but not when exported. White background can be
+% removed in Inkscape.
